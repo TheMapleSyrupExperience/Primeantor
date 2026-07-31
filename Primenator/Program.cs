1c38109709d3fc7c802e89468363995aae3f1c71
MapleSyrup <smithwpro@gmail.com>
2026-07-30 22:09:30 -0400

    Created parser for ingesting CSV primes to simple list

--- Primenator/Program.cs
+++ Primenator/Program.cs
@@ -1,29 +1,78 @@
 ﻿
+using System.Collections;
 using System.Runtime.CompilerServices;
 
 class Primeinator
-{
+{ 
     static void Main()
     {
-        //Grabs our program directory and initializes the string to the location of our PrimesList and 
+        //Grabs our program directory and initializes the string to the location of our PrimesList then reads that file into rawPrimes
         string primesPath = Directory.GetCurrentDirectory() + "\\PrimeList.txt";
         string rawPrimes = File.ReadAllText(primesPath);
+
+        Console.WriteLine("test");
+
+        List<int> parsedPrimes = NumberParse(rawPrimes);
+
         Console.WriteLine("How many numbers would you like to evaluate?");
 
-        string primeSplits = Promptuser();
+        string primeSplitsInput = "";
 
-        while (primeSplits == "")
+        do
         {
-            primeSplits = Promptuser();
+            primeSplitsInput = PromptUser();
         }
+        while (primeSplitsInput == "");
 
-        Console.WriteLine("thank you!");
+        int primeDivisons = Convert.ToInt16(primeSplitsInput);
+
+        for (int i = 0; i < parsedPrimes.Count; i++)
+        {
+            Console.WriteLine(parsedPrimes[i]);
+        }
         
+
     }
 
-    static private string Promptuser()
+    //logic to convert our CSV primes to a simple, ordered list of primes
+    static private List<int> NumberParse(string rawPrimes)
+    {
+        Queue<char> intermediatePrime = new Queue<char>();
+        string completePrime = "";
+        List<int> parsedPrimes = new List<int>();
+
+        for (int i = 0; i + 1 <= rawPrimes.Length; i++)
+        {
+            if (rawPrimes[i] == ',')
+            {
+                //convert queue to string, add that as converted int32 to list then clear queue
+
+                completePrime = string.Concat(intermediatePrime);
+                parsedPrimes.Add(Convert.ToInt32(completePrime));
+
+                for (int j = 0; intermediatePrime.Count != 0; j++)
+                {
+                    intermediatePrime.Dequeue();
+                }
+                completePrime = "";
+            }
+            else if (rawPrimes[i] == ' ') 
+            {
+
+            }
+            else
+            {
+                intermediatePrime.Enqueue(rawPrimes[i]);
+            }
+        }
+        return parsedPrimes;
+    }
+
+    static private string PromptUser()
     {
         string? userInput = Console.ReadLine();
+
+        //TODO: LIMIT THE LENGTH OF WHAT THE USER CAN INPUT HERE TO LESS THAN THE TOTAL NUMBER OF PRIMES
 
         if (userInput is null)
         {
@@ -60,5 +109,4 @@ class Primeinator
         return false;
     }
 }
-
 
